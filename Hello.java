dcdcc1536d40b099f82a57b595a4892f96c58f18
KIRUTHIKA285 <137389906+KIRUTHIKA285@users.noreply.github.com>
2023-06-22 14:10:44 +0530

    Add files via upload

---
+++ Hello.java
@@ -0,0 +1,38 @@
+package prac; 
+import java.util.Scanner;
+
+public class Hello{
+	public static void main(String[]args)
+	{
+		System.out.println("Enter your mobile number");
+		Scanner sc = new Scanner(System.in);
+		String input = sc.next();
+		
+		System.out.println(numberOrNot(input));
+		if(numberOrNot(input)&&(input.length()==10))
+		{
+			System.out.println("Good!!!You have entered valid mobile number");
+		}
+		else
+		{
+			System.out.println("Sorry!!!You have entered invalid mobile number.Try again");
+		}
+	}
+	static boolean numberOrNot(String input)
+	{
+		try
+		{
+			Long.parseLong(input);
+		}
+		catch(NumberFormatException ex)
+		{
+			ex.printStackTrace();
+			  return false;
+		}
+		return true;
+				
+		}
+		
+		
+	
+}
